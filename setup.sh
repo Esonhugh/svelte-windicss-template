f8a50c17156ccba760bee94a2078bc904d6a6620
Esonhugh <esonhughoutside@gmail.com>
2022-05-08 20:18:57 +0800

    add setup.sh and add workflows

---
+++ setup.sh
@@ -0,0 +1,12 @@
+function build_by_local () {
+  echo "dependencies sync ........."
+  npm install
+  echo "build apps ..........."
+  npm run build
+  cp -r ./public/ ./output
+  echo "build success: at dir `pwd`/output"
+}
+
+function build_by_action () {
+  git clone ${Project_url} --branch app
+}
